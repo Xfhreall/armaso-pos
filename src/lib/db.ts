5a792dcbe6ae57e48c9c4aef627775a3c496104d
Xfhreall <farelrizqi377@gmail.com>
2026-01-27 11:35:55 +0700

    feat: prisma client

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
@@ -6,12 +6,8 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 function createPrismaClient() {
-  const accelerateUrl = process.env.DATABASE_URL
-  if (!accelerateUrl) {
-    throw new Error('DATABASE_URL environment variable is required')
-  }
   return new PrismaClient({
-    accelerateUrl,
+    accelerateUrl: process.env.DATABASE_URL,
   }).$extends(withAccelerate())
 }
 
