e93c1bb9689deb9569ba95783caf6f6a96c37261
Xfhreall <farelrizqi377@gmail.com>
2026-01-27 12:09:35 +0700

    feat: make on pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const config = defineConfig({
   plugins: [
@@ -17,6 +18,34 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
+      manifest: {
+        name: 'Nyawit POS',
+        short_name: 'Nyawit',
+        description: 'Point of Sales System for Nyawit',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon'
+          },
+          {
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192'
+          },
+          {
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512'
+          }
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
